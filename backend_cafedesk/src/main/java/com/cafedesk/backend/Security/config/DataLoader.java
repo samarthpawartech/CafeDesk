3032663eb93f115bb6cd0886cd10e3ae3147cd92
samarthpawartech <samarth.pawar.tech@gmail.com>
2026-04-29 00:08:02 +0530

    Dataloader file Updation

--- backend_cafedesk/src/main/java/com/cafedesk/backend/Security/config/DataLoader.java
+++ backend_cafedesk/src/main/java/com/cafedesk/backend/Security/config/DataLoader.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
